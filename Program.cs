31834c901e089e7e01fd9f9763be04d86261b98d
Kavrakov Shavqat <kavrakovshavqatinfo@gmail.com>
2021-08-09 14:55:43 +0500

    change Решение задач 03-08-2020

--- Program.cs
+++ Program.cs
@@ -1,77 +1,66 @@
 ﻿using System;
 
-namespace problems1
+namespace problems
 {
     class Program
     {
-        
-        // Задача 1
         static void Main(string[] args)
         {
+          // Задача 1
         double a=double.Parse(Console.ReadLine());
          double b=double.Parse(Console.ReadLine());
-         double result=Math.Sqrt(a*b);
-         Console.WriteLine($"результат = {Math.Round(result,2)}");
-        }
-       /*
+         double result1=Math.Sqrt(a*b);
+         Console.WriteLine($"результат = {Math.Round(result1,2)}");
+    {  
         // Задача 2
-        static void Main(string[] args) 
-        {
+        Console.WriteLine();
      double A=double.Parse(Console.ReadLine());
      double B=double.Parse(Console.ReadLine());
      double C=double.Parse(Console.ReadLine());
      double AC=Math.Abs((A-C));
      double BC=Math.Abs((B-C));
-     double result=Math.Round((AC+BC),2);
+     double result2=Math.Round((AC+BC),2);
      Console.WriteLine($"AC = {Math.Round(AC,2)}");
      Console.WriteLine($"BC = {Math.Round(BC,2)}");
-     Console.WriteLine($"AC+BC = {result}");
-        }*/
-
-     /*
+     Console.WriteLine($"AC+BC = {result2}");
+        }
+      {    
       // Задача 3
-      static void Main(string[] args)
-      {
+      Console.WriteLine();
       double x1=double.Parse(Console.ReadLine());
       double y1=double.Parse(Console.ReadLine());
       double x2=double.Parse(Console.ReadLine());
       double y2=double.Parse(Console.ReadLine());
-      double result=Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
-      Console.WriteLine($"результат = {Math.Round(result,2)}");
+      double result3=Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
+      Console.WriteLine($"результат = {Math.Round(result3,2)}");
       }
-      */
-      /*
-      // Задача 4
-      static void Main(string[] args)
-      {
+      { 
+         // Задача 4
+         Console.WriteLine();
          Console.Write("Число: "); 
-          int ab=int.Parse(Console.ReadLine());
-          int a=ab / 10;
-          int b=ab % 10;
-          int ba=b*10+a;
-          Console.WriteLine($"Результат: {ba}");
-      }
-      */
-      /*
-      // Задача 5
-      static void Main(string[] args)
-      {
+          int cd=int.Parse(Console.ReadLine());
+          int c=cd / 10;
+          int d=cd % 10;
+          int result4=d*10+c;
+          Console.WriteLine($"Результат: {result4}");
+     }
+      {  
+         // Задача 5
+         Console.WriteLine();
           Console.Write("N = ");
        int N=int.Parse(Console.ReadLine());
-       int result=N / 60;
-       Console.WriteLine($"Результат: {result}");
-
+       int result5=N / 60;
+       Console.WriteLine($"Результат: {result5}");
       }
-      */
-    /*
-      // Задача 6
-      static void Main(string[] args)
       {
+          // Задача 6
+          Console.WriteLine();
           Console.Write("K = ");
         int K=int.Parse(Console.ReadLine());
-        int result=K % 7 ;
-        Console.WriteLine($"Номер дня недели: {result}");
+        int result6=K % 7 ;
+        Console.WriteLine($"Номер дня недели: {result6}");
       }
-    */
+    
     }
+}
 }
